dafa6001c4e70a45cafe0d9b26d784fd1bf43d44
sampaath <kannaboinasampath5@gmail.com>
2018-01-04 22:59:22 -0500

    yesyes

---
+++ Collections&Sorting/src/BinarySearch.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+
+	 int[] a= {1,3,5,6,8,9,10,12};
+	 static Scanner sc = new Scanner(System.in);
+	
+	
+
+	public static void main(String[] args) 
+	{
+		 System.out.println(" enter the number you want check ");
+		 
+		 BinarySearch b = new BinarySearch();	 
+		 b.show();
+		
+
+	}
+	
+	int l = 0, k = a.length;
+	
+	// int[] a= {1,3,5,6,8,9,10,12};
+	
+	int x = 12;
+	
+ public String show() 
+ 	{
+	 System.out.println(a.length);
+	while(l<=k)
+		{
+		 int m = (k+l)/2;
+		 
+		 	if(x==a[m])
+		 	{
+		 		System.out.println("  true");
+		 		return "true";
+		 	}
+		 
+			else
+			{
+			if(x>a[m])
+			{
+			l= m+1;
+			/*System.out.println("  1");*/
+			}
+			if(x<a[m])
+			 {
+				k= m-1;
+//				System.out.println("  2");
+			 }
+			
+		}
+			 
+		}
+		
+			return "Sampath";
+
+
+}
+}
+
